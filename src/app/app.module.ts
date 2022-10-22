2a5eaeeb5528a6c1515a4aa41d38cc1a09c30f95
dmirak <dmirakian1023@gmail.com>
2022-10-22 19:35:44 -0400

    Adds storage service and settings saving

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,18 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+import { IonicStorageModule } from '@ionic/storage-angular';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
-  enabled: environment.production,
-  // Register the ServiceWorker as soon as the application is stable
-  // or after 30 seconds (whichever comes first).
-  registrationStrategy: 'registerWhenStable:30000'
-})],
+  imports: [BrowserModule, IonicModule.forRoot(), IonicStorageModule.forRoot(), AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsPage implements OnInit {
 
-  constructor() { }
+  public darkMode: boolean;
+  public rotateMap: boolean;
+  public accessibilityMode: boolean;
 
-  ngOnInit() {
+  private isInitialized = false;
+
+  constructor(private storageService: StorageService) { }
+
+  async ngOnInit() {
+    await this.storageService.init();
+
+    this.darkMode = await this.storageService.get('darkMode');
+    this.rotateMap = await this.storageService.get('rotateMap');
+    this.accessibilityMode = await this.storageService.get('accessibilityMode');
+
+    // ionChange events seem to be running before ngOnInit can finish which
+    // causes undefined to be stored for some inputs
+    this.isInitialized = true;
   }
+
+  saveSettings() {
+    if (this.isInitialized) {
+      this.storageService.set('darkMode', this.darkMode);
+      this.storageService.set('rotateMap', this.rotateMap);
+      this.storageService.set('accessibilityMode', this.accessibilityMode);
+    }
+  }
+
+  darkModeChanged() {
+    this.saveSettings();
+  }
+
+  rotateMapChanged() {
+    this.saveSettings();
+  }
+
+  accessibilityModeChanged() {
+    this.saveSettings();
+  }
+
 
 }
