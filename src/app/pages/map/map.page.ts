ecc383fe5ad5fe5ed093630680fb1c47b64d529c
unknown <tj29@students.calvin.edu>
2022-10-27 14:00:32 -0400

    testing

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -9,12 +9,17 @@ declare var google: any;
   templateUrl: 'map.page.html',
   styleUrls: ['map.page.scss'],
 })
+
+
+
 export class MapPage {
   map: any;
+  isItemAvailable = false;
+  items = [];
 
   @ViewChild('map', { read: ElementRef, static: false }) mapRef: ElementRef;
 
-  constructor() {}
+  constructor() { }
 
   ionViewDidEnter() {
     this.showMap();
@@ -28,5 +33,40 @@ export class MapPage {
       disableDefaultUI: true,
     };
     this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+    // const input = document.getElementById('pac-input');
+    // const searchBox = new google.maps.places.searchBox(input);
+    // this.map.addListener('bouds_changed', function () {
+    //   searchBox.setBounds(this.map.getBounds());
+    // });
+    // const markers = [];
+    // searchBox.addListener('places_changed', function () {
+
+    // })
   }
+
+  initializeItems() {
+    this.items = ['SB104', 'SB302', 'HH108', 'NH101', 'Gamma 5'];
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() !== '') {
+      this.isItemAvailable = true;
+      // eslint-disable-next-line arrow-body-style
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    } else {
+      this.isItemAvailable = false;
+    }
+  }
+
+
 }
+
