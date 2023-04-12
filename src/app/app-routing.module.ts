10e5e96771cc5f0d8affdb78974265e25c0a0590
tj29@students.calvin.edu <tj29@students.calvin.edu>
2023-04-12 16:50:17 -0400

    changes during meeting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./pages/map/map.module').then(m => m.MapPageModule)
+    loadChildren: () => import('./components/tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
     path: '',
